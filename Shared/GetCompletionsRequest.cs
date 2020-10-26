93c8d3ab201910ac6ece435e2fac99397147e88b
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2020-10-26 15:08:14 +0000

    Get completions

---
+++ Shared/GetCompletionsRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace blazoract.Shared
+{
+    public class GetCompletionsRequest
+    {
+        public string Code { get; set; }
+        public int LineNumber { get; set; }
+        public int Column { get; set; }
+    }
+}
