0151543f4d2a3ae93136fbc6a73dbbb51f3e1edd
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2020-10-26 11:15:21 +0000

    Use a single Notebook instance per ID

--- Client/Data/NotebookService.cs
+++ Client/Data/NotebookService.cs
@@ -16,6 +16,8 @@ namespace blazoract.Client.Data
         private ILocalStorageService _storage;
         private HttpClient _http;
 
+        private Dictionary<string, Notebook> _inMemoryNotebooks = new Dictionary<string, Notebook>();
+
         public NotebookService(ILocalStorageService storage, HttpClient http)
         {
             _storage = storage;
@@ -39,7 +41,13 @@ namespace blazoract.Client.Data
 
         public async Task<Notebook> GetById(string id)
         {
-            return await _storage.GetItemAsync<Notebook>(id);
+            if (!_inMemoryNotebooks.TryGetValue(id, out var result))
+            {
+                result = await _storage.GetItemAsync<Notebook>(id);
+                _inMemoryNotebooks[id] = result;
+            }
+
+            return result;
         }
 
         public async Task<string> CreateNewNotebook()
@@ -49,12 +57,13 @@ namespace blazoract.Client.Data
             var notebook = new Notebook(id, title);
             notebook.Cells = new List<Cell>() { new Cell("", 0) };
             await _storage.SetItemAsync(id, notebook);
+            _inMemoryNotebooks[id] = notebook;
             return id;
         }
 
         public async Task<Notebook> AddCell(string id, string content, CellType type, int position)
         {
-            var notebook = await _storage.GetItemAsync<Notebook>(id);
+            var notebook = await GetById(id);
             notebook.Cells.Insert(position, new Cell(content, position, type));
             await _storage.SetItemAsync(id, notebook);
             OnChange.Invoke();
