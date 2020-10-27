c8153bada280d17cf665c2eed589c901919de8aa
Safia Abdalla <safia@microsoft.com>
2020-10-27 11:34:34 -0700

    Migrate completions API to Azure Functions

--- Api/KernelFunctions.cs
+++ Api/KernelFunctions.cs
@@ -15,6 +15,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using System.Net.Http;
 using Newtonsoft.Json;
+using Microsoft.CodeAnalysis.Text;
 
 namespace blazoract.Api
 {
@@ -27,7 +28,7 @@ namespace blazoract.Api
         }
 
         [FunctionName("RunCode")]
-        public async Task<IActionResult> Run(
+        public async Task<IActionResult> RunCode(
             [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "code/run")] HttpRequest req,
             ILogger log)
         {
@@ -51,6 +52,73 @@ namespace blazoract.Api
             });
 
             return new OkObjectResult(result);
+        }
+
+        [FunctionName("GetCompletions")]
+        public async Task<IActionResult> GetCompletions(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "code/completions")] HttpRequest req,
+            ILogger log)
+        {
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            var completionRequest = JsonConvert.DeserializeObject<GetCompletionsRequest>(requestBody);
+
+            var request = await _kernel.SendAsync(new RequestCompletions(completionRequest.Code, new LinePosition(completionRequest.LineNumber - 1, completionRequest.Column - 1)));
+            var result = Array.Empty<Suggestion>();
+            request.KernelEvents.Subscribe(x =>
+            {
+                if (x is CompletionsProduced completions)
+                {
+                    result = completions.Completions.Select(c => new Suggestion
+                    {
+                        Label = c.DisplayText,
+                        InsertText = c.InsertText,
+                        Kind = Enum.TryParse<CompletionItemKind>(c.Kind, out var parsedKind) ? parsedKind : CompletionItemKind.Property,
+                        Documentation = c.Documentation,
+                    }).ToArray();
+                }
+            });
+            return new OkObjectResult(result);
+        }
+
+        // TODO: Avoid duplication by changing MonacoRazor to target 3.0 or moving shared types to seperate package.
+        public enum CompletionItemKind : int
+        {
+            Method = 0,
+            Function = 1,
+            Constructor = 2,
+            Field = 3,
+            Variable = 4,
+            Class = 5,
+            Struct = 6,
+            Interface = 7,
+            Module = 8,
+            Property = 9,
+            Event = 10,
+            Operator = 11,
+            Unit = 12,
+            Value = 13,
+            Constant = 14,
+            Enum = 15,
+            EnumMember = 16,
+            Keyword = 17,
+            Text = 18,
+            Color = 19,
+            File = 20,
+            Reference = 21,
+            Customcolor = 22,
+            Folder = 23,
+            TypeParameter = 24,
+            User = 25,
+            Issue = 26,
+            Snippet = 27
+        }
+
+        public class Suggestion
+        {
+            public string Label { get; set; }
+            public string InsertText { get; set; }
+            public CompletionItemKind Kind { get; set; }
+            public string Documentation { get; set; }
         }
     }
 }
