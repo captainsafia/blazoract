9fa4465f36ddebb71ac20d3a5c75ee663415bef8
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2020-10-28 13:49:44 +0000

    General mechanism for displaying different kinds of structured output

--- Shared/ExecuteResult.cs
+++ Shared/ExecuteResult.cs
@@ -2,8 +2,11 @@
 {
     public class ExecuteResult
     {
-        public object Output { get; set; }
+        // Tell the client which kind of object we're sending, so it can try to JSON-deserialize it as that type
+        public string OutputType { get; set; }
+        public string OutputJson { get; set; }
 
-        public string CommandFailedMessage { get; set; }
+        // In case the client can't deserialize this type (or the server can't serialize it), fall back on a string representation
+        public string OutputToString { get; set; }
     }
 }
